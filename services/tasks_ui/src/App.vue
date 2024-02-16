1472960c54da0b6159a025b19c49abda83b2f23b
Joris Jansen <joris97jansen@gmail.com>
2024-02-16 11:28:18 +0100

    Implement Amplify in UI (Vue)

--- services/tasks_ui/src/App.vue
+++ services/tasks_ui/src/App.vue
@@ -1,30 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <authenticator username-alias="email" :login-mechanisms="['email']">
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+</script>
---
+++ services/tasks_ui/src/aws-exports.js
@@ -0,0 +1,9 @@
+export default {
+    Auth: {
+        Cognito: {
+            region: 'us-east-1',
+            userPoolId: 'us-east-1_6a3VuMaI6',
+            userPoolClientId: 'rmp9iub1p0n48v71o0nl7gik9'
+        }
+    }
+  };
